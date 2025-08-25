d0bab080fb61a89669d3ed2b466ee4e9987e811e
joel543-sv <joelpanassiti@gmail.com>
2025-08-25 20:25:53 -0300

    setud configuration

--- test.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from main import suma 
-class TestPrueba(unittest.Testcase):
-    def test_suma (self):
-        self.assertequal(suma(2,3),5)
-        if __name__== "__main__":
-            unittest.main()
